7dc91f4dfae0780a54888452f9800fa1fa6e1fa5
Honsage <musatov.i.a@edu.mirea.ru>
2025-12-13 14:10:47 +0300

    feat(presentation): add raw markup, style and controller of task item

---
+++ src/main/java/ru/honsage/dev/gitpm/presentation/controllers/TaskItemController.java
@@ -0,0 +1,20 @@
+package ru.honsage.dev.gitpm.presentation.controllers;
+
+import javafx.event.ActionEvent;
+
+public class TaskItemController {
+    public void onPriorityLow(ActionEvent actionEvent) {
+    }
+
+    public void onPriorityMedium(ActionEvent actionEvent) {
+    }
+
+    public void onPriorityHigh(ActionEvent actionEvent) {
+    }
+
+    public void onEdit(ActionEvent actionEvent) {
+    }
+
+    public void onDelete(ActionEvent actionEvent) {
+    }
+}
