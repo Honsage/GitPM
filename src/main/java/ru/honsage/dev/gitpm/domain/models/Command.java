0310501e6e887a25a23cc162a8448ea3337c1c40
Honsage <musatov.i.a@edu.mirea.ru>
2025-12-16 00:57:53 +0300

    fix(domain/models): add order field to Command to provide sequence ordering inside Script

--- src/main/java/ru/honsage/dev/gitpm/domain/models/Command.java
+++ src/main/java/ru/honsage/dev/gitpm/domain/models/Command.java
@@ -1,5 +1,6 @@
 package ru.honsage.dev.gitpm.domain.models;
 
+import ru.honsage.dev.gitpm.domain.exceptions.ExceptionFactory;
 import ru.honsage.dev.gitpm.domain.valueobjects.ExecutableCommand;
 import ru.honsage.dev.gitpm.domain.valueobjects.CommandId;
 import ru.honsage.dev.gitpm.domain.valueobjects.WorkingDir;
@@ -10,14 +11,35 @@ public class Command {
     private final CommandId id;
     private WorkingDir workingDir;
     private ExecutableCommand executableCommand;
+    private int order;
+
+    public Command(
+            CommandId id,
+            WorkingDir workingDir,
+            ExecutableCommand executableCommand,
+            int order
+    ) {
+        this.id = Objects.requireNonNull(id);
+        this.workingDir = Objects.requireNonNull(workingDir);
+        this.executableCommand = Objects.requireNonNull(executableCommand);
+        validateOrder(order);
+        this.order = order;
+    }
 
     public Command(
             CommandId id,
             WorkingDir workingDir,
             ExecutableCommand executableCommand
     ) {
-        this.id = Objects.requireNonNull(id);
-        this.workingDir = Objects.requireNonNull(workingDir);
-        this.executableCommand = Objects.requireNonNull(executableCommand);
+        this(id, workingDir, executableCommand, 0);
+    }
+
+    private void validateOrder(int order) {
+        if (order < 0) {
+            throw ExceptionFactory.validation(
+                    "Command order must be non-negative integer",
+                    "Command.order"
+            );
+        }
     }
 }
