33478563828ae704860b3b9e8d1998a8369f2e55
Honsage <musatov.i.a@edu.mirea.ru>
2025-12-17 17:00:00 +0300

    feat(application/services): create ScriptExecutionService

---
+++ src/main/java/ru/honsage/dev/gitpm/application/services/ScriptExecutionService.java
@@ -0,0 +1,45 @@
+package ru.honsage.dev.gitpm.application.services;
+
+import ru.honsage.dev.gitpm.domain.models.Command;
+import ru.honsage.dev.gitpm.domain.ports.CommandExecutor;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ScriptExecutionService {
+    private final CommandExecutor executor;
+
+    private final Map<String, Process> runningProcesses = new ConcurrentHashMap<>();
+
+    public ScriptExecutionService(CommandExecutor commandExecutor) {
+        this.executor = commandExecutor;
+    }
+
+    public void runScript(String scriptId, Command command) {
+        if (runningProcesses.containsKey(scriptId)) {
+            throw new IllegalStateException("Script is already running");
+        }
+
+        Process process = executor.execute(
+                command.getWorkingDir().toPath(),
+                command.getExecutableCommand().toString()
+        );
+
+        runningProcesses.put(scriptId, process);
+
+        process.onExit().thenRun(() -> runningProcesses.remove(scriptId));
+    }
+
+    public void stopScript(String scriptId) {
+        Process process = runningProcesses.get(scriptId);
+        if (process == null) return;
+
+        process.destroy();
+
+        runningProcesses.remove(scriptId);
+    }
+
+    public boolean isRunning(String scriptId) {
+        return runningProcesses.containsKey(scriptId);
+    }
+}
