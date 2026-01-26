4041927992fe1cdb9931e3b2571e1c206a1af357
Honsage <musatov.i.a@edu.mirea.ru>
2026-01-26 20:10:40 +0300

    feat(infrastructure/utils): support remote url opening with different browsers

--- src/main/java/ru/honsage/dev/gitpm/infrastructure/utils/OSUtils.java
+++ src/main/java/ru/honsage/dev/gitpm/infrastructure/utils/OSUtils.java
@@ -8,8 +8,13 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
+import java.util.List;
 
 public class OSUtils {
+    public static boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("win");
+    }
+
     public static void copyToClipBoard(String text) {
         Clipboard clipboard = Clipboard.getSystemClipboard();
         ClipboardContent content = new ClipboardContent();
@@ -29,13 +34,55 @@ public class OSUtils {
     }
 
     public static void openInBrowser(String url) {
+        // TODO: support browserType transfer to method
+        String browserType = "default";
+        if (isWindows()) {
+            openInBrowserWin(browserType, url);
+        } else {
+            openInBrowserUnix(browserType, url);
+        }
+    }
+
+    private static void openInBrowserUnix(String browserType, String url) {
+        launchDefaultBrowser(url);
+    }
+
+    private static void openInBrowserWin(String browserType, String url) {
+        switch (browserType) {
+            case "yandex" -> launchBrowserWin("browser", url);
+            case "chrome" -> launchBrowserWin("chrome", url);
+            case "firefox" -> launchBrowserWin("firefox", url);
+            case "opera" -> launchBrowserWin("opera", url);
+            default -> launchDefaultBrowser(url);
+        }
+    }
+
+    private static void launchDefaultBrowser(String url) {
         try {
-            Desktop desktop = Desktop.getDesktop();
-            desktop.browse(new URI(url));
+            Desktop.getDesktop().browse(new URI(url));
         } catch (Exception e) {
             Alert alert = new Alert(Alert.AlertType.ERROR,
                     "Не удалось открыть браузер: " + e.getMessage());
             alert.showAndWait();
         }
+    }
+
+    private static void launchBrowserWin(String browserExe, String url) {
+        try {
+            Runtime.getRuntime().exec(new String[]{"cmd", "/c", "start", browserExe, url});
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR,
+                    "Не удалось открыть браузер: " + e.getMessage());
+            alert.showAndWait();
+        }
+    }
+
+    private static List<String> getBrowserExeList() {
+        return List.of(
+                "browser",
+                "chrome",
+                "firefox",
+                "opera"
+        );
     }
 }
